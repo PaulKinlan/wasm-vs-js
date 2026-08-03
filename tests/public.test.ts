76d4f99a843047bbf8cb21f97837769829e3ff12
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-03 17:01:43 +0100

    Allow commit OID links in public test suite

--- tests/public.test.ts
+++ tests/public.test.ts
@@ -211,6 +211,10 @@ Deno.test("public pages contain no inline script, inline style, or remote asset"
         "",
       )
       .replaceAll(
+        /https:\/\/github\.com\/PaulKinlan\/wasm-vs-js\/commit\/[a-f0-9]+/g,
+        "",
+      )
+      .replaceAll(
         /https:\/\/github\.com\/PaulKinlan\/wasm-vs-js\/blob\/(?:30b41425227dc139304f25942d0be0d933fa28c9|9691f0e8353a221880f365712a1ebbec18b7dde4)\/[a-zA-Z0-9._/-]+(?:#L[0-9]+(?:-L[0-9]+)?)?/g,
         "",
       );
