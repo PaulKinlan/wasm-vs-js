73cf56c49916d9c9cb98f7bac5453fccbc5db5e0
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-03 11:37:31 +0100

    Point inventory at remediated records

--- tests/public.test.ts
+++ tests/public.test.ts
@@ -205,7 +205,7 @@ Deno.test("public pages contain no inline script, inline style, or remote asset"
         "",
       )
       .replaceAll(
-        /https:\/\/github\.com\/PaulKinlan\/wasm-vs-js\/blob\/62c29101344f1f7da2c17146718770ce50478621\/[a-zA-Z0-9._/-]+(?:#L[0-9]+(?:-L[0-9]+)?)?/g,
+        /https:\/\/github\.com\/PaulKinlan\/wasm-vs-js\/blob\/d4062f860464d2a91cf0dda8e0c7f01a813caf75\/[a-zA-Z0-9._/-]+(?:#L[0-9]+(?:-L[0-9]+)?)?/g,
         "",
       );
     assert(!/https?:\/\//i.test(withoutAllowedLinks), `${path} has an unexpected remote URL`);
