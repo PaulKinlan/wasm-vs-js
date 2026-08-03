cea79c82dd94f619352898f79903c3ee436ffa76
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-03 11:00:56 +0100

    Refresh immutable text inventory links

--- tests/public.test.ts
+++ tests/public.test.ts
@@ -205,7 +205,7 @@ Deno.test("public pages contain no inline script, inline style, or remote asset"
         "",
       )
       .replaceAll(
-        /https:\/\/github\.com\/PaulKinlan\/wasm-vs-js\/blob\/0295659347c915dca2bd6e34f8acbfeb35eafc71\/[a-zA-Z0-9._/-]+(?:#L[0-9]+(?:-L[0-9]+)?)?/g,
+        /https:\/\/github\.com\/PaulKinlan\/wasm-vs-js\/blob\/856ee12fb89702e0a56ad9137c828827ca0d8435\/[a-zA-Z0-9._/-]+(?:#L[0-9]+(?:-L[0-9]+)?)?/g,
         "",
       );
     assert(!/https?:\/\//i.test(withoutAllowedLinks), `${path} has an unexpected remote URL`);
