daf1113e8192e3eac04c419bed65fd179f185848
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-03 13:34:02 +0100

    Implement frozen base tooling minify and format workload

---
+++ benchmarks/base/tooling-minify-format/browser-entry.ts
@@ -0,0 +1,4 @@
+export { generateFixture } from "./generator.ts";
+export { transformJs } from "./engine.ts";
+export { instantiateToolingWasm } from "./wasm.ts";
+export { LANGUAGES, OPERATIONS } from "./contract.ts";
