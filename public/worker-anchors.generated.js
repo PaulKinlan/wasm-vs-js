73aca960136b2298791bec504c8848b0f3a6fdf4
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 00:27:52 +0100

    Rebind todomvc conversion (source-commit 942ace5e71b7ed538daad479b235e589eee698e0)

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "c47c242d8b65d04da46ae7b6234a5b3f857eea220bf4812c9a2f4de14112df77",
+    buildManifest: "be3f88aa744c05373bd1b0ecc8fa551d91cbefaeae604377742c5bb29c54906a",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
