49cf395fe3de79a0b6a66898a30eea6706d23327
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 00:50:08 +0100

    Settle rebind pins to HEAD

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "d977a40a471c3daef4251c09b620f6a41267041bed7bbedcde336d4f36177c2c",
+    buildManifest: "72bcd3acc9dcf45418335822cff347184554bd44ec35682ce5de6b23d79922e5",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
