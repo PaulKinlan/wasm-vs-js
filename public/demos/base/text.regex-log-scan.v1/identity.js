89d46614e7dae4aea37f86aa055a234bb2e35ab5
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-04 21:44:56 +0100

    Rebind manifests for slice 1

--- public/demos/base/text.regex-log-scan.v1/identity.js
+++ public/demos/base/text.regex-log-scan.v1/identity.js
@@ -4,7 +4,7 @@ export const IDENTITY = {
   "sourceCommit": "3fc5d9a756f074aa3fa75bf24585b8915619dc24",
   "rawSha256": {
     "registration": "e01d7682f4e2e0c466d90a6769e832af88562fd9af79ac5fb50022a99cbd46fb",
-    "buildManifest": "db10251470abd36cad652cf747bbed81e07d51c558e00cd0c29041cff1d3f9fb",
+    "buildManifest": "f535abdb8d0f8ed404d656024aed12c397086a5267f4d3599671ab5402ac5738",
     "inputManifest": "546b40c57f1a75db3b4f37feb8b38d5a3b75054de997394abdaa578953c9924a",
     "outputManifest": "e7599e0e453ec6059269073533a28cf6ab37c39109eee3792e8065cb6ec562ac",
     "wasm": "7c9e86f13085de36a1a4a4c5f93162f5b5fb0b96b1d36dfc01b5775a0d832fbe",
