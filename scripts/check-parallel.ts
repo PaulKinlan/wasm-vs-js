6848992bf5027c107ce21bea89ecfefc5dd48798
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-04 02:42:50 +0100

    check-parallel: rebalance flock to DENO_JOBS=12 (gltf no longer critical)

--- scripts/check-parallel.ts
+++ scripts/check-parallel.ts
@@ -186,7 +186,7 @@ await Promise.all([
   runStage({
     name: "test-readers",
     args: ["test", "--parallel", ...testArgs, ...readerTests],
-    env: { ...testEnv, DENO_JOBS: "8" },
+    env: { ...testEnv, DENO_JOBS: "12" },
   }),
   ...HEAVY_READERS.map((file) =>
     runStage({
