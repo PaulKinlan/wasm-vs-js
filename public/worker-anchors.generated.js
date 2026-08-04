7075ca53cda8921a1b3c8ad9efb441b3504a2695
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 00:31:28 +0100

    Rebind cad-mesh restyle (routes regen)

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "be3f88aa744c05373bd1b0ecc8fa551d91cbefaeae604377742c5bb29c54906a",
+    buildManifest: "8e5cce8e992af33cf12925748c87d03fba95d39242539d6d1ed2d1cdf887221a",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
