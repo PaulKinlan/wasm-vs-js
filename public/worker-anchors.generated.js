e78f19f2a5ede71893dd560fd6755d67c218e781
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 01:39:37 +0100

    Rebind all manifests for multilang wave 2 additions

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "e0dba91c80394b24e8aaec4d9bb629e6854e882e299c29b1ecb9946aaa4d54cd",
+    buildManifest: "76d0682759a1744f3aac1a8845614bf319d51133de094866dd4981e1c7390f76",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
