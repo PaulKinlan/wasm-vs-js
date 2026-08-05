301125cf28624d7af493ca0bc18df784f7c76135
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 01:26:56 +0100

    Rebind archive-zip conversion

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "830629de8e6853e8ed3145d8e1045f064ee8b07a1f3ffccf3314a92ae65d2410",
+    buildManifest: "e43c5a6d6d70900207b95e729962708bf891b15081eaa3fcf7f66fda19d3f909",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
