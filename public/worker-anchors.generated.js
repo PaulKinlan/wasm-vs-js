03ee8a61e105daa356e3e4aa528fb2e1ca1eac46
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 01:17:41 +0100

    Rebind wave-2 batch 2a manifest pins

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "f684c3fb8a764c7ad961838af48a49ed5b86a4b3bd48a24f1ee2920e3522c3db",
+    buildManifest: "ad5a96de0514d01de68214341e44793b57b249650dc3eb9d0aee2e1f2dac6868",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
