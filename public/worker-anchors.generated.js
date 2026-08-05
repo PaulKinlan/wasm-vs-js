c863ac16bcec10f50a83169ae7dfd0015e03394c
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 01:54:34 +0100

    settle pass 3

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "94a8d99f51bbe461206fdce366f0058f8650f2ea47b984b4e358ec9ca6ae4537",
+    buildManifest: "b41bcb993894ebfd3d71a94675e559168485a087f5820b7a7e34924358545160",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
