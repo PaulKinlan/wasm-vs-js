58e38d8cd8fa630c27787849b24b3fd8ee19ba3b
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-05 02:24:07 +0100

    Rebind after batch-2b merge

--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "3dbf551b683fb2fafebd37ce20a1de4917aa241348df638c05bed09aaf19caaf",
+    buildManifest: "bb39163137f71611a5424c8ac4ff9452a1938e2462adc76ac1ed05c2a8d58d16",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
