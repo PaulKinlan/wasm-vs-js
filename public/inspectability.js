a9583cc54f1451e33a7b725bd721954cbe3464e6
Paul Kinlan <paul.kinlan@gmail.com>
2026-08-06 13:10:17 +0100

    rebind pass 2

--- public/inspectability.js
+++ public/inspectability.js
@@ -17,7 +17,7 @@ const LOCAL_DOWNLOADS = new Map([
     "/artifacts/sum-u32/sum-u32.wasm",
   ],
   [
-    `public/artifacts/audio-fft/build-manifest.json|6d9bb4eb154b058365223674e73c85e19abcad2860dd17cbf693491d36db6245`,
+    `public/artifacts/audio-fft/build-manifest.json|3d7a6a54b33e1007a6594c698398fd9011c898abd7aa44fce9efb56a6a620cd1`,
     "/artifacts/audio-fft/build-manifest.json",
   ],
   [
@@ -29,7 +29,7 @@ const LOCAL_DOWNLOADS = new Map([
     "/artifacts/audio-fft/reference-output.f32le",
   ],
   [
-    `public/artifacts/audio-fir/build-manifest.json|2dc719c08461e9f7f9ddbf9f0218d4bf92dc0c5d2f856e0170a095864cf2cb1b`,
+    `public/artifacts/audio-fir/build-manifest.json|30c8b7424b1fddaae2214c1096ff4c62f617c4b0ac0f7e4452ee1072ca2f41ed`,
     "/artifacts/audio-fir/build-manifest.json",
   ],
   [
@@ -41,7 +41,7 @@ const LOCAL_DOWNLOADS = new Map([
     "/artifacts/audio-fir/reference-output.f32le",
   ],
   [
-    `public/artifacts/audio-stft/build-manifest.json|2b4fbf6942f9a2784b061717820ebb9b49ee3b7f14ad30f5a7b4276c7d042e16`,
+    `public/artifacts/audio-stft/build-manifest.json|511586d13fdaf95df17d0d9d7771685dcc1b9a187cce9d3f485b7b7531e15091`,
     "/artifacts/audio-stft/build-manifest.json",
   ],
   [
--- public/worker-anchors.generated.js
+++ public/worker-anchors.generated.js
@@ -15,7 +15,7 @@ export const WORKER_ANCHORS = Object.freeze({
     wasm: "37ee977839172da08b9b7f2dca782694b9c6cdd41c06673b6a011efce972f521",
   }),
   "text-gc-document-edit": Object.freeze({
-    buildManifest: "08ae963a5197ae8ac0a221d3db5839b68c57c1056d98aae8432abcee174d4e56",
+    buildManifest: "f9ce6d2f1e59a9fce0e6739bf44a9543f2e669a3e1083ab589b449dec38602eb",
     fixture: "45b117b798a6b35f42a3b5a541f5112bb7100510735e17650fee11ee6c8d6075",
     fixtureManifest: "8b4145b0d91f6eb086e7c3cabe1bf1bb9a647c9e7771ad0f824005141ebe5681",
     js: "466731002f3c005583c26592f4495cb80cc42639e40b1f0c6de236f7676eb443",
